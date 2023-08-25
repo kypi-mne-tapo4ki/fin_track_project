6c7a6b08c7bfd35a95f730256e11798f4141f609
Dmitrii Ermolaev <d.l.ermolaev@yandex.ru>
2023-08-25 15:32:53 +0200

    Refactoring

--- core_ledger/views.py
+++ core_ledger/views.py
@@ -6,6 +6,18 @@ from .models import ExpenseCategory, Transaction, Income
 from .forms import TransactionForm, IncomeForm
 
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('core_ledger:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/registration_form.html', {'form': form})
+
+
 def index(request):
     transactions = Transaction.objects.all()
     incomes = Income.objects.all()
@@ -35,18 +47,6 @@ def add_transaction(request):
         'core_ledger/add_transaction.html',
         {'form': form, 'incomes': incomes, 'expense_categories': expense_categories}
     )
-
-
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('core_ledger:index')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/registration_form.html', {'form': form})
 
 
 def add_category(request):
