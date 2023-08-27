d138f5e264c08ba32928819844bf6ba5153987e4
Dmitrii Ermolaev <d.l.ermolaev@yandex.ru>
2023-08-27 22:29:20 +0200

    Added transaction functionality and the ability to delete transactions

--- core_ledger/views.py
+++ core_ledger/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.views.generic.detail import DetailView
+from django.shortcuts import get_object_or_404
 
 from .models import ExpenseCategory, Transaction, Income
 from .forms import TransactionForm, IncomeForm
@@ -21,35 +23,40 @@ def register(request):
 def index(request):
     transactions = Transaction.objects.all()
     incomes = Income.objects.all()
+    expense_category = ExpenseCategory.objects.all()
     return render(request,
                   'core_ledger/index.html',
-                  {'transactions': transactions, 'incomes': incomes})
+                  {
+                      'transactions': transactions,
+                      'incomes': incomes,
+                      'expense_category': expense_category,
+                  }
+                  )
 
 
 def add_transaction(request):
-    incomes = Income.objects.all()
-    expense_categories = ExpenseCategory.objects.all()
-
     if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
             transaction = form.save(commit=False)
-            transaction.expense_category.amount += transaction.amount
-            transaction.expense_category.save()
-            transaction.income_source.amount -= transaction.amount
-            transaction.income_source.save()
+            transaction.take_from_income()
+            transaction.add_to_expenses()
             transaction.save()
             return redirect('core_ledger:index')
     else:
         form = TransactionForm()
-    return render(
-        request,
-        'core_ledger/add_transaction.html',
-        {'form': form, 'incomes': incomes, 'expense_categories': expense_categories}
-    )
+    return render(request, 'core_ledger/add_transaction.html', {'form': form})
 
 
-def add_category(request):
+def delete_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    if request.method == 'POST':
+        transaction.delete()
+        return redirect('core_ledger:index')
+    return render(request, 'core_ledger/confirm_delete.html', {'transaction': transaction})
+
+
+def add_expense_category(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         ExpenseCategory.objects.create(name=name)
@@ -66,3 +73,9 @@ def add_income(request):
     else:
         form = IncomeForm()
     return render(request, 'core_ledger/add_income.html', {'form': form})
+
+
+class CategoryTransactionsView(DetailView):
+    model = ExpenseCategory
+    template_name = 'core_ledger/category_transactions.html'
+    context_object_name = 'category'
