1428454a68a1409e31fae9b481fd64838b496f0a
Dmitrii Ermolaev <d.l.ermolaev@yandex.ru>
2023-08-29 23:47:41 +0200

    feat: Add authorization function.

--- core_ledger/views.py
+++ core_ledger/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.views.generic.detail import DetailView
 from django.views.generic import ListView
 from django.shortcuts import get_object_or_404
@@ -10,18 +9,11 @@ from .models import ExpenseCategory, Transaction, IncomeCategory, Income
 from .forms import TransactionForm, IncomeForm
 
 
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('core_ledger:index')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/registration_form.html', {'form': form})
+def home(request):
+    return render(request, 'base.html')
 
 
+@login_required(login_url='users:login')
 def index(request):
     transactions = Transaction.objects.filter(date__lte=timezone.now()).order_by("date").reverse()[:5]
     incomes = IncomeCategory.objects.all()
